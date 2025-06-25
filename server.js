a664a61338491d6d2d539b327df1b2d450eca283
shiprachaubey <chaubeyshipra73@gmail.com>
2025-06-25 17:50:05 +0530

    chnages

--- server.js
+++ server.js
@@ -1,37 +1,4 @@
 
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-// const dotenv = require("dotenv");
-// const { connectDB } = require('./config/db');
-// const authRoutes = require('./routes/authRoutes');
-// const pdfRoutes = require('./routes/PDFRoutes');
-// const fileUpload = require('express-fileupload');
-
-// connectDB();
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-// app.use(fileUpload({ useTempFiles: true }));
-
-
-// dotenv.config();
-
-// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-
-// // Routes
-// app.use('/api/auth', authRoutes);
-// app.use('/api/dietplans', pdfRoutes);
-
-// app.get('/health', (req, res) => {
-//   res.send('✅ Little Voices API is running.');
-// });
-
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 
 require("dotenv").config();
 const express = require('express');
